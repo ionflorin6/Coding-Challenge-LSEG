5de7b445106b22c3309dfa95df0dde7de14a3a4a
Ion Florin <florin_cristian_2000@yahoo.com>
2024-03-28 00:15:17 +0200

    add error handling

--- csv_processor_module.py
+++ csv_processor_module.py
@@ -1,32 +1,80 @@
 import csv
 import random
 import statistics
+from dateutil.parser import parse as parse_date
+
+
+def is_date(string, fuzzy=False):
+    try:
+        parse_date(string, fuzzy=fuzzy)
+        return True
+    except ValueError:
+        return False
+
 
 class CSV_processor:
-    def __init__(self, file_path, output_path, consecutive_data_points):
+    def __init__(self, file_path, output_dir, file_name, consecutive_data_points):
         self.consecutive_data_points = consecutive_data_points
-        self.output_path = "output/" + output_path
+        self.output_path = output_dir + "/out_" + file_name
+        self.output_dir = output_dir
+        self.file_name = file_name
         self.file_path = file_path
         self.stock_price_mean = 0
         self.number_of_rows = 0
-        self.init_csv_processor()
 
     def init_csv_processor(self):
-        with open(self.file_path, 'r') as csv_file:
-            content = csv.reader(csv_file)
-            sum = 0
-            number_of_rows = 0
-            for row in content:
-                number_of_rows += 1
-                sum += float(row[2])
-            self.stock_price_mean = sum/number_of_rows
-            self.number_of_rows = number_of_rows
+        if not self.file_path.endswith('.csv'):
+            print(f"File [{self.file_path}] is not csv file.")
+            return -1
+
+        try:
+            with open(self.file_path, 'r') as csv_file:
+                content = csv.reader(csv_file)
+                sum = 0
+                number_of_rows = 0
+                price = 0
+
+                for row in content:
+                    if len(row) != 3:
+                        print(f"In file [{self.file_path}], csv file format is incorrect.")
+                        return -1
+
+                    if not is_date(row[1]):
+                        print(f"In file [{self.file_path}], csv file date format is incorrect [{row[1]}].")
+                        return -1
+
+                    number_of_rows += 1
+                    try:
+                        price = float(row[2])
+                    except ValueError:
+                        print(f"In file [{self.file_path}], csv file stock price format is incorrect."
+                              f"\nCould not convert stock price to float [{price}].")
+                        return -1
+
+                    sum += price
+                if number_of_rows == 0:
+                    print(f"Csv file [{self.file_path}] is empty.")
+                    return -1
+
+                self.stock_price_mean = sum/number_of_rows
+                self.number_of_rows = number_of_rows
+        except FileNotFoundError:
+            print(f"File [{self.file_path}] not found.")
+            return -1
+
+        except OSError as e:
+            print(f"File [{self.file_path}] error({e.errno}): {e.strerror}")
+            return -1
+
+        else:
+            return 0
 
     def get_random_data_points(self):
         with open(self.file_path, 'r') as csv_file:
             content = csv.reader(csv_file)
             random_data_point = random.randint(0, self.number_of_rows - self.consecutive_data_points - 1)
             output_list = []
+
             for i, line in enumerate(content):
                 if i == random_data_point:
                     for _ in range(0, self.consecutive_data_points):
@@ -36,6 +84,7 @@ class CSV_processor:
     
     def get_clean_data_points(self, data_points_list):
         clean_data_points = []
+
         for data_point in data_points_list:
             clean_data_points.append(float(data_point[2]))
         return clean_data_points
@@ -44,16 +93,18 @@ class CSV_processor:
         clean_data_points_list = self.get_clean_data_points(data_points_list)
         data_mean = statistics.mean(clean_data_points_list)
         standard_deviation = statistics.stdev(clean_data_points_list)
-        min_threshold = data_mean - (0.1 * standard_deviation)
-        max_threshold = data_mean + (0.1 * standard_deviation)
+        min_threshold = data_mean - (2 * standard_deviation)
+        max_threshold = data_mean + (2 * standard_deviation)
         outliers_list = [['Stock-ID', 'Timestamp', 'actual stock price at that timestamp', 'mean of 30 data points',
                          'actual stock price – mean', '% deviation over and above the threshold']]
+
         for row in data_points_list:
             if float(row[2]) < min_threshold or float(row[2]) > max_threshold:
                 row.append(data_mean)
                 row.append(self.stock_price_mean)
                 row.append(abs((data_mean - float(row[2])) / 100))
                 outliers_list.append(row)
+
         with open(self.output_path, 'w', newline='') as outliers_csv:
             outliers_content = csv.writer(outliers_csv)
             outliers_content.writerows(outliers_list)
--- main.py
+++ main.py
@@ -1,23 +1,66 @@
 import csv_processor_module
 import os
+import sys
+import argparse
 
-cosecutive_data_points = 30
+consecutive_data_points = 30
 ROOT_DIR = "./stock_price_data_files"
 
-def stock_exchange_parser(files_number):
-    stock_exchanges = os.listdir(ROOT_DIR)
-    for stock_exchange in stock_exchanges:
-        csv_files = os.listdir(ROOT_DIR + "/" + stock_exchange)
-        if len(csv_files) != 0:
-            if files_number > len(csv_files):
-                print("Files_number bigger than existing csv files")
-            for csv_file_index in range(0, min(files_number, len(csv_files))):
-                file_path = ROOT_DIR + "/" + stock_exchange + "/" + csv_files[csv_file_index]
-                csv_processor = csv_processor_module.CSV_processor(file_path, csv_files[csv_file_index],
-                                                                   cosecutive_data_points)
-                random_data_set_rows = csv_processor.get_random_data_points()
-                csv_processor.get_outliers(random_data_set_rows)
-        else:
-            print("No csv files present")
+parser = argparse.ArgumentParser()
 
-stock_exchange_parser(2)
+parser.add_argument("-fn", "--files-number", default="1", dest="filesnumber", help="Number of files to be sampled for each Stock Exchange")
+parser.add_argument("-i", "--input-directory", default="./stock_price_data_files", dest="inputdirectory", help="Root directory for stock exchanges")
+parser.add_argument("-o", "--output-directory", default="./output", dest="outputdirectory", help="Output directory for csv files")
+
+args = parser.parse_args()
+
+def stock_exchange_parser(files_number, input_directory, output_directory):
+    if files_number < 0:
+        print(f"Invalid file number [{files_number}].")
+        sys.exit()
+
+    stock_exchanges = os.listdir(input_directory)
+
+    if len(stock_exchanges) == 0:
+        print(f"No stock exchanges in input directory [{input_directory}].")
+        sys.exit()
+
+    for stock_exchange in stock_exchanges:
+        stock_exchange_path = input_directory + "/" + stock_exchange
+
+        if not os.path.isdir(stock_exchange_path):
+            print(f"Skipping [{stock_exchange_path}] as it is not a directory.")
+            continue
+
+        csv_files = os.listdir(stock_exchange_path)
+        if len(csv_files) == 0:
+            print(f"No csv files present in [{(stock_exchange_path)}]"
+                  f"\nContinuing with the rest of the stock exchanges.")
+            continue
+
+        if files_number > len(csv_files):
+            print(f"File number [{files_number}] is greater than number of csv files [{len(csv_files)}] for"
+                  f"stock exchange [{stock_exchange}]."
+                  f"\nContinuing with [{len(csv_files)}] files.")
+
+        for csv_file_index in range(0, min(files_number, len(csv_files))):
+            file_path = stock_exchange_path + "/" + csv_files[csv_file_index]
+            csv_processor = csv_processor_module.CSV_processor(file_path, output_directory, csv_files[csv_file_index],
+                                                               consecutive_data_points)
+
+            if csv_processor.init_csv_processor() != 0:
+                continue
+            random_data_set_rows = csv_processor.get_random_data_points()
+            csv_processor.get_outliers(random_data_set_rows)
+
+
+if __name__ == "__main__":
+    if not os.path.isdir(args.inputdirectory):
+        print(f"Input directory [{args.inputdirectory}] doesn't exist.")
+        sys.exit()
+
+    if not os.path.isdir(args.outputdirectory):
+        print(f"Output directory [{args.outputdirectory}] doesn't exist.")
+        sys.exit()
+
+    stock_exchange_parser(int(args.filesnumber), args.inputdirectory, args.outputdirectory)
